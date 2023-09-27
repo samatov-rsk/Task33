70789c2dc62226d083ff88bf26fc3976805f61be
samatov-rsk <samativrsk@gmail.com>
2023-09-27 16:45:45 +0300

    final project

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -1,9 +1,7 @@
 package ru.kata.spring.boot_security.demo.models;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
